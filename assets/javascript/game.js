e5327d0a186d5cb6071af3a5d32971a7d7d50fb2
Jennifer King <jenniferking@Jennifers-MacBook-Air.local>
2017-05-30 19:01:00 -0600

    updating another version of Hangman Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,124 +1,114 @@
-
 //Arrays and Variables
 
-var chosenWord = "";
 var spellingList = ["afternoon", "again", "country", "cousin", "home", "hope", "horse", "school", "second", "see", "seen", "sent", "set", "she", "shoes", "should", "show", "sister", "six", "small", "snow", "so", "some", "something", "sometime", "soon", "spelling", "spring", "started", "stay", "stayed", "store", "story", "street", "summer", "Sunday", "supper", "sure", "table", "take", "teacher", "tall", "ten", "than", "thank", "that", "the", "their", "them", "there", "these", "they", "thing", "think", "third", "three", "through", "time", "table", "take", "teacher", "writing", "written"];
-var spellingWord = "";
-var letters = [];
+var chosenWord = "";
+var lettersinWord = [];
 var numberBlanks = 0;
-var wrongLetters = 0;
-var blanksAndSuccess = [];
-var checkLetters = true;
-
+var blanksAndHits = [];
+var wrongLetters = [];
 
 //counters
 var winCount = 0;
 var lossCount = 0;
-var guessLeft = 3;
+var guessLeft = 9;
 
+//============================================================================================
 //Functions
 
-function StartHangman(){
+function startHangman(){
 chosenWord = spellingList[Math.floor(Math.random() * spellingList.length)];
-letters = chosenWord.split("");
-numberBlanks = letters.length;
-//reset
-guessLeft = 10;
-wrongLetters = [];
-blanksAndSuccess = [];
-//Populate blanks and successes with the right number of blanks
-for (var i = 0; i < numberBlanks; i++){
-  blanksAndSuccess.push("__");
-//document.getElementByid("wordtoGuess").innerHTML=blanksAndSuccess.join(" ");
+console.log(chosenWord);
+
+lettersinWord = chosenWord.split("");
+numberBlanks = lettersinWord.length;
+
+  for (var i = 0; i < numberBlanks; i++) {
+    blanksAndHits.push("_");
+  }
+  
+  console.log(blanksAndHits);
+
+  document.getElementById("word").innerHTML = blanksAndHits.join("");
+  document.getElementById("guess").innerHTML = guessLeft;
+  document.getElementById("Wins").innterHTML = winCount;
+  document.getElementById("losses").innerHTML = lossCount;
+
 }
 
-function CheckLetters(letter){
+function checkLetters(letter){
   //check letters
-  var letters = false;
+  var isLetterInWord = false;
   for (var i = 0; i<numberBlanks; i++){
-    if (chosenWord [i]==letter){
-       letters=true;
-       alert("the letter is in the word")
+    if (chosenWord [i] == letter){
+       isLetterInWord=true;
     }
   }
- if(letters){
+  console.log("the letter is in the word");
+
+ if(isLetterInWord){
     for(var i = 0; i < numberBlanks; i++){
       if(chosenWord[i] == letter){
-        blanksAndSuccess[i] = letter;
+        blanksAndHits[i] = letter;
       }
     }
-
- }
+  }
+ 
 //letter wasn't found
   else{
-    wrongletters.push(letter)
-    numGuess--;
+    wrongLetters.push(letter)
+    guessLeft--;
   }
-  }
+console.log(blanksAndHits);
+
 }
 
-//console.log(blanksAndSuccess);
+function roundComplete(){
+  console.log("Win Count: " + winCount + " | Loss Count: " + lossCount +  "| Guesses Left: " + guessLeft);
 
+document.getElementById("guess").innerHTML = guessLeft;
+document.getElementById("word").innerHTML = blanksAndHits.join(" ");
+//document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" "); 
+//check if won
+  if (lettersinWord.toString() == blanksAndHits.toString()) {
 
-function roundCompleted(){
-console.log ("Win Count:" + winCount + "|lossCount:" + lossCount +"Guesses Left" + guessLeft);
+  //dispay the win counter in the HTML
+  document.getElementById("Wins").innerHTML = winCount;
+  document.getElementById("word").innerHTML = chosenWord;
+  
+  alert("You Win!");
+  startHangman();
 }
 
-//check if winner won
-/*if (letters.to String()==blanksAndSuccess.to String()){
-  winCount++
-  alert("YOU WON!");
-}
-*/
+else if (guessLeft == 0) {
+  lossCount++;
+  alert("You lost");
 
-//console.log for debugging
+document.getElementById("losses")
+  startHangman();
+}
+
+}
+
+
+//================================================================
+
+startHangman();
+
+document.onkeyup = function(event){
+  var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+console.log(letterGuessed);
+}
+
 console.log(chosenWord);
-console.log(letters);
-console.log(numberBlanks);
-//console.log(blanksAndSuccess)
 
-//Main Process
-StartHangman();
-
-
-
-//keyclicks
-
-/*document.onkeyup=function(event){
-  var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase;
-  console.log(letterGuessed);
-}
-*/
-
-
-/*
-
-window.onload = function () {
-
-
-}
-
-function pickWord(){
-rand = Math.floor(Math.random()*spellingWord.length);
-    spelligWord = spellingList[rand];
-console.log(spellingWord);
-}
-
-alert("This is an alert box!");
-console.log("this is set up correctly.")    
-
-
-
-Show lives
-   comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-      showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === geusses.length) {
-        showLives.innerHTML = "You Win!";
-      }
-    }
-
+/*var buttons=document.querySelectorAll(".btn");
+console.log(buttons);
+buttons.forEach(function(button){
+  button.addEventListener("click",function(event){
+    console.log(event.target.text);
+    this.classList.push('disabled');
+  })
+})
 */
